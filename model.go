9c8100badba790a2666018c16d2091460da26920
晓杰 <61188737+jess-weigou@users.noreply.github.com>
2021-03-19 19:46:30 +0800

    complete the model

--- model.go
+++ model.go
@@ -1,8 +1,20 @@
 package main
 
 type Input struct {
-    UserName string    `json:"user_name"`
-    PassWord string     `json:"pass_word"`
+    Id int
+    AccountPhone string    `json:"account_phone"`
+    PassWord string     `json:"password"`
+    DingDingAccount string `json:"dingding_account"`
+}
+type StaffInterface struct{
+    Id int
+    StaffPhone string
+    StaffStatus string
+}
+type CompanyInterface struct {
+    Id int
+    CompanyId string
+    CompanyName string
 }
 type SelfDetail struct {
     StaffPhone string `json:"staff_phone"`
@@ -15,4 +27,37 @@ type SelfDetail struct {
     StaffCharacter string `json:"staff_character"`
     StaffExperience string `json:"staff_experience"`
     StaffSchool string `json:"staff_school"`
+}
+type EmploymentStatus struct {
+    FileId int `json:"file_id"`
+    StaffPhone string `json:"staff_phone"`
+    CompanyId string `json:"company_id"`
+    DeptId string `json:"dept_id"`
+    StaffId string `json:"staff_id"`
+    WorkAttendance string `json:"work_attem_dance"`
+    WorkPerformance string `json:"work_performance"`
+    WorkSalary int `json:"work_salary"`
+    WorkInTime string `json:"work_in_time"`
+    WorkPost string `json:"work_post"`
+    CreaterName string `json:"creater_name"`
+}
+type Performance struct {
+    Id int `json:"id"`
+    FileId int `json:"file_id"`
+    PerformData string
+    PerformScore int
+}
+
+type Attendance struct {
+    Id int `json:"id"`
+    FileId int `json:"file_id"`
+    AttendanceData string
+    AttendanceSituation string
+}
+
+type Mistake struct {
+    Id int `json:"id"`
+    FileId int `json:"file_id"`
+    MistakeData string
+    MistakeDetail string
 }
